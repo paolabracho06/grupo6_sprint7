1077ef11dfcbc462a28c4365640877e99589d864
paolabracho06 <paolabracho683@gmail.com>
2022-02-22 12:37:34 -0300

    subo archivos del sprint6

---
+++ entregables/cuenta.md
@@ -0,0 +1,21 @@
+cuenta admin
+------------
+email: admin@dh.com
+pass: 11111
+
+||||||||||||||
+
+cuenta vendedor
+---------------
+
+email: mayorista@dh.com
+pass: 11111
+
+
+|||||||||||||||||||||||||
+
+cuenta comprador
+---------------
+
+email: individual@dh.com
+pass: 11111
---
+++ src/controller/controllerCategory.js
@@ -0,0 +1,35 @@
+const db = require("../database/models");
+const Category = db.Cat;
+const controllerCategory = {
+    list: (req,res)=>{
+        Category.findAll()
+        .then(result=>{
+            res.render("admin/product/listCategory.ejs",{result})
+        })
+    },
+    edit: (req,res)=>{
+        Category.findByPk(req.params.id)
+        .then(category=>{
+            res.render("admin/product/editCategory.ejs",{category})
+        })
+    },
+    editProcess: (req,res)=>{
+        Category.update({
+            name: req.body.name
+        },{
+            where: {id: req.params.id}
+        })
+        .then(result=> res.redirect('/category'));
+    },
+    add: (req,res)=>{
+        res.render("admin/product/addCategory.ejs")
+    },
+    addProcess:(req,res)=>{
+        Category.create({
+            name: req.body.name
+        })
+        .then(result => res.redirect('/category'))
+    }
+}
+
+module.exports = controllerCategory
