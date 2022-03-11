92aefa0582dd8dd2d2231ff752fdf3a7dc8e4ce0
JesusNeyra <jesua.park@gmail.com>
2022-03-11 20:14:38 -0300

    Validacion de password en register completado

--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -1,35 +1,61 @@
 const registerForm=document.querySelector('#registerform');
-const inputs=document.querySelectorAll('#registerform input')
+const inputs=document.querySelectorAll('#registerform input');
 //^
 
-const expresion={
+const validadores={
     first_name:/^[a-zA-ZÀ-ÿ\s]{2,20}$/,
     last_name:/^[a-zA-ZÀ-ÿ\s]{2,20}$/,
-    email:/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+$/
+    email:/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+$/,
+    pass:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{4,15}$/
 }
 const campos={
     first_name:false,
     last_name:false,
-    email:false
+    email:false,
+    pass:false
 }
 
 const validarForm=(e)=>{
     
     switch (e.target.name) {
         case 'first_name':
-            validarCampo(expresion.first_name,e.target,'first_name');
+            validarCampo(validadores.first_name,e.target,'first_name');
             console.log('first_name')       
             break;
         case 'last_name':
-            validarCampo(expresion.last_name,e.target,'last_name')
+            validarCampo(validadores.last_name,e.target,'last_name')
             console.log('estas en last_name')
             break;
         case 'email':
-            validarCampo(expresion.email,e.target,'email')
+            validarCampo(validadores.email,e.target,'email')
             console.log('estas en email')
             break;
+        case 'pass':
+            validarCampo(validadores.pass,e.target,'pass');
+            validarPass1();
+            console.log('pass')       
+            break;
+        case 'pass1':
+            validarPass1();
+            console.log('pass1')       
+            break;
+            
         default:
             break;
+    }
+};
+const validarPass1=()=>{
+    console.log("contraseñas iguales")
+    const inputPass=document.getElementById('contrasenia');
+    const inputPass1=document.getElementById('contrasenia1');
+    if(inputPass.value!==inputPass1.value){
+        document.getElementById(`errors-pass1`).classList.remove('div-error')
+        document.getElementById(`errors-pass1`).classList.add('div-error-block')
+        campo[pass]=false
+    }else{
+        document.getElementById(`errors-pass1`).classList.add('div-error')
+        document.getElementById(`errors-pass1`).classList.remove('div-error-block')
+        campos[pass]=true
     }
 };
 
